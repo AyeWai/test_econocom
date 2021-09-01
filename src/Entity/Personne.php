9a36b8bd890df18c71f8356b7dfd4ddb8d48a406
Chris <cs.simon@live.fr>
2021-09-01 15:05:59 +0200

    Formulaire

---
+++ src/Entity/Personne.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PersonneRepository::class)
+ */
+class Personne
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datenaissance;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getDatenaissance(): ?\DateTimeInterface
+    {
+        return $this->datenaissance;
+    }
+
+    public function setDatenaissance(\DateTimeInterface $datenaissance): self
+    {
+        $this->datenaissance = $datenaissance;
+
+        return $this;
+    }
+}
---
+++ src/Service/PersonneService.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Personne;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class PersonneService extends AbstractController{
+
+    public function persistPersonne(Request $request, ValidatorInterface $validator){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $firstname = $request->request->get("firstName");
+        $lastname = $request->request->get("lastName");
+        $mail = $request->request->get("email");
+        $status = $request->request->get("status");
+
+        $Personne = new Personne();
+        $Personne->setFirstname($firstname);
+        $Personne->setLastname($lastname);
+        $Personne->setMail($mail);
+        $Personne->setStatus($status);
+
+        $errors = $validator->validate($Personne);
+        if (count($errors) > 0) {
+            //return new Response((string) $errors, 400);
+            return $this->render('home/errors.html.twig', ['errors' => $errors,]);
+        }
+        elseif(count($errors) == 0){
+
+
+            // tell Doctrine you want to (eventually) save the Product (no queries yet)
+            $entityManager->persist($Personne);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+        }
+    }
+
+    public function sendEmail(MailerInterface $mailer, Request $request ): void
+    {   
+        $firstname = $request->request->get("firstName");
+        $lastname = $request->request->get("lastName");
+        $mail = $request->request->get("email");
+        $status = $request->request->get("status");
+
+        $email = (new TemplatedEmail())
+            ->from('chris-dev@hotmail.com')
+            ->to($mail)
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Confirmation de prise de Personne sur chris-dev.fr')
+            ->text('Sending emails is fun again!')
+            ->htmlTemplate('mails/reply.html.twig')
+
+            ->context([
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+                'status' => $status,
+            ]);
+
+        $mailer->send($email);
+
+        // ...
+    }
+}
